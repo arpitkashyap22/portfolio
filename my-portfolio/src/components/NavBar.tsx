83d2f115eaa6d940cd5098a60c1158e23f600e9a
Arpit Kashyap <arpitkashyap2000@gmail.com>
2025-04-18 07:56:51 +0000

    react init

---
+++ my-portfolio/src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.css'; // Optional: Add custom styles here
+
+const NavBar: React.FC = () => {
+    return (
+        <nav className="navbar">
+           This is NavBar
+        </nav>
+    );
+};
+
+export default NavBar;
